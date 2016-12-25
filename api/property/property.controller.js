f0babff83cb7bf254068ecb55f6a4c846bef1e7e
Walaa <walaa@yamsafer.me>
2016-12-25 17:29:00 +0200

    stable async await

--- api/property/property.controller.js
+++ api/property/property.controller.js
@@ -7,6 +7,7 @@
 
 var _ = require('lodash');
 var request = require('request');
+var range = require('node-range');
 var $q = require('q');
 
 var async = require('asyncawait/async');
@@ -20,21 +21,32 @@ exports.index = async(function(req, res) {
     // var page_limit = res;
     var size = req.query.size || 50
     var page_limit = req.query.page || 10
-    var wait = await (getAndIndex(size, page_limit))
-    return res.json({ data: wait })
+
+    var pages_arr = range(0,page_limit).toArray().map(function(i) { return i })
+    console.log(pages_arr);
+
+
+    // var all = await (pages_arr.map(async(function(page) {
+    //               var wait = await (getAndIndex(size, page))
+    //               return wait;
+    //             })));
+    // return res.json(all);
 });
 
 
 var getAndIndex = async(function(size, page_number) {
     var properties = await (getPropertiesByElastic(size, page_number));
-    return properties
+
     var results = await (_.map(properties, async(function(property, index) {
-        property = await (property);
         console.warn('### ' + page_number + '##' + index + ' ### Start indexing property ...');
-        console.info('.... Name: ' + property._source.en.name);
         console.log('.... ID#: ' + property._id);
-        return await (indexPorpertyHealth(property._id));
+        var res = await (indexPorpertyHealth(property._id));
+        if (res) {
+          return res;
+        }
+        return property._id
     })));
+
     return results;
 
 })
@@ -42,10 +54,14 @@ var getAndIndex = async(function(size, page_number) {
 var indexPorpertyHealth = function(property_id) {
     //- config.endpoint: it's a api gateway to exucute lambda function for indexing property content health
     var deferred = $q.defer();
+    console.log(config.endpoint, 'config.endpoint');
     request({
         url: config.endpoint + '?property_id=' + property_id,
-        method: 'GET'
+        method: 'POST'
     }, function(error, response, body) {
+        if (response) {
+            console.log(response.statusCode, 'response.statusCode', property_id);
+        }
         if (!error && response.statusCode == 200) {
             var data = JSON.parse(body);
             deferred.resolve(data)
@@ -62,20 +78,21 @@ var getPropertiesByElastic = function(size, page_number) {
         host: elastic.host
     });
 
-    var response = await(client.search({
+    var response = await (client.search({
         index: 'properties',
         type: 'property',
-        size: 1000,
-        from: 0,
+        size: size,
+        from: page_number,
         body: {
-            "fields" : ["en.name", "en.id"],
+            "fields": ["en.name", "en.id"],
             "query": {
                 "match": { "en.supplier_id": "1" }
-            }
+            },
+            "sort": { "created_at": { "order": "asc" } }
         }
     }));
 
     if (response && response.hits && response.hits.hits) {
-      return response.hits.hits;
+        return response.hits.hits;
     }
 }
